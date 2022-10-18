ccadf16230e1574d5222fa01e0895c4c2de7dd45
saik1998 <107240573+saik1998@users.noreply.github.com>
2022-10-18 11:48:47 +0530

    update commit

--- app/src/main/java/com/firstapp/exte_livedata_task/Fragment_B.java
+++ app/src/main/java/com/firstapp/exte_livedata_task/Fragment_B.java
@@ -74,8 +74,8 @@ public class Fragment_B extends Fragment {
             @Override
             public void onChanged(@Nullable Object o) {
                 viewModel.getUserMutableLiveData();
-                loadData();
-                saveData();
+//                loadData();
+//                saveData();
 
 
             }
@@ -85,54 +85,54 @@ public class Fragment_B extends Fragment {
         return view;
 
     }
-    private void loadData() {
-        // method to load arraylist from shared prefs
-        // initializing our shared prefs with name as
-        // shared preferences.
-        SharedPreferences sharedPreferences= getContext().getSharedPreferences("shared preference",Context.MODE_PRIVATE);
-
-
-//        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
-
-        // creating a variable for gson.
-        Gson gson = new Gson();
-
-        // below line is to get to string present from our
-        // shared prefs if not present setting it as null.
-        String json = sharedPreferences.getString("courses", null);
-
-        // below line is to get the type of our array list.
-        Type type = new TypeToken<List<String>>() {}.getType();
-
-        // in below line we are getting data from gson
-        // and saving it to our array list
-        dataModelList = gson.fromJson(json, type);
-        // checking below if the array list is empty or not
-        if (dataModelList == null)
-        {
-            // if the array list is empty
-            // creating a new array list.
-            dataModelList = new ArrayList<>();
-        }
-    }
-
-    @SuppressLint("NewApi")
-    private void saveData() {
-        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-
-        Gson gson = new Gson();
-        String json = gson.toJson(dataModelList);
-
-        editor.putString("courses", json);
-        // below line is to apply changes
-        // and save data in shared prefs.
-        editor.apply();
-        // after saving data we are displaying a toast message.
-        Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
-    }
+//    private void loadData() {
+//        // method to load arraylist from shared prefs
+//        // initializing our shared prefs with name as
+//        // shared preferences.
+//        SharedPreferences sharedPreferences= getContext().getSharedPreferences("shared preference",Context.MODE_PRIVATE);
+//
+//
+////        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+//
+//        // creating a variable for gson.
+//        Gson gson = new Gson();
+//
+//        // below line is to get to string present from our
+//        // shared prefs if not present setting it as null.
+//        String json = sharedPreferences.getString("courses", null);
+//
+//        // below line is to get the type of our array list.
+//        Type type = new TypeToken<List<String>>() {}.getType();
+//
+//        // in below line we are getting data from gson
+//        // and saving it to our array list
+//        dataModelList = gson.fromJson(json, type);
+//        // checking below if the array list is empty or not
+//        if (dataModelList == null)
+//        {
+//            // if the array list is empty
+//            // creating a new array list.
+//            dataModelList = new ArrayList<>();
+//        }
+//    }
+//
+//    @SuppressLint("NewApi")
+//    private void saveData() {
+//        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("shared preferences", Context.MODE_PRIVATE);
+//
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//
+//
+//        Gson gson = new Gson();
+//        String json = gson.toJson(dataModelList);
+//
+//        editor.putString("courses", json);
+//        // below line is to apply changes
+//        // and save data in shared prefs.
+//        editor.apply();
+//        // after saving data we are displaying a toast message.
+//        Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
+//    }
 }
 
 
